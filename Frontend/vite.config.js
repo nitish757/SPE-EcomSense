99dca1dae6063112a9f2346c2da7233880469ee5
nitish <nitish468mahapatre@gmail.com>
2025-05-18 14:45:55 +0530

    conf dc kube

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      host: '0.0.0.0',
       '/products': 'http://localhost:8082',
       '/api/inventory': 'http://localhost:8083',
       // '/forecasts': 'http://localhost:8083'
